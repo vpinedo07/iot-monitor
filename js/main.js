b8ba72dd99a3fc765df8986083fc8604138ddb5d
vpinedo <74575048+vpinedo07@users.noreply.github.com>
2025-04-30 12:33:10 -0600

    Archivos originales

---
+++ js/main.js
@@ -0,0 +1,44 @@
+function fetchDeviceData() {
+    fetch("http://18.206.163.32/api/devices")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("No se pudo obtener la información");
+        }
+        return response.json();
+      })
+      .then(data => {
+        const tbody = document.getElementById("data-table-body");
+        const statusHeading = document.getElementById("main-status");
+  
+        tbody.innerHTML = ""; // Limpiar tabla
+  
+        if (data.devices[0].status != "") {
+          // Mostrar el status de la primera fila
+          statusHeading.textContent = `Estado actual: ${data.devices[0].status}`;
+        } else {
+          statusHeading.textContent = "No hay datos disponibles";
+        }
+
+        console.table(data.devices);
+  
+        data.devices.forEach(device => {
+          const row = `
+            <tr>
+              <td>${device.id}</td>
+              <td>${device.name}</td>
+              <td>${device.ip}</td>
+              <td>${device.status}</td>
+              <td>${device.date}</td>
+            </tr>
+          `;
+          tbody.innerHTML += row;
+        });
+      })
+      .catch(error => {
+        console.error("Error al obtener los datos:", error);
+      });
+  }
+  
+  setInterval(fetchDeviceData, 20000);
+  document.addEventListener("DOMContentLoaded", fetchDeviceData);
+  
